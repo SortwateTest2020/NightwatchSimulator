0e1ce9e5ed75198e4ca2536657fabaae59df3657
SHINNYCHOU <41077008+SHINNYCHOU@users.noreply.github.com>
2020-04-19 20:53:05 +0800

    Update function1_boundary_08.test.js

--- case/function1_boundary/function1_boundary_08.test.js
+++ case/function1_boundary/function1_boundary_08.test.js
@@ -10,11 +10,11 @@ const calc_button = 'body > div.container.grid-sm.s-content > div.column.col-12 
 
 // test case 08
 let input = {}
-input["year"] = -1000
-input["month"] = 1
-input["date"] = 1
-input["span"] = 1
-let expected = "无效年份输入"
+input["year"] = -271821
+input["month"] = 5
+input["date"] = 5
+input["span"] = 5
+let expected = "-271821年5月10日星期一"
 module.exports = {
     'test_case_08':  function (browser) {
       browser
