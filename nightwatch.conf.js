0498ec4c611dea6303af8dd030dc1700f68e6644
WXM99 <wxmxl@hotmail.com>
2020-04-19 12:56:28 +0800

    Mod settings

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,14 +2,14 @@
 module.exports = {
   src_folders: ['case'],
   'webdriver': {
-    'server_path': './chromedriver', // 浏览器 driver 的 bin 执行路径
-    'start_process': true, // 需要启动 driver
-    'port': 9515 // driver 启动的端口, 一般是 9515 或 4444
+    'server_path': './chromedriver', 
+    'start_process': true,
+    'port': 9515 
   },
   'test_settings': {
     'default': {
       'desiredCapabilities': {
-        'browserName': 'chrome' // 浏览器的名字叫 safari
+        'browserName': 'chrome'
       }
     }
   }
